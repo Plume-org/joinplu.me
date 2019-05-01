ea22dfede3c8d5d6fe4e1358bf9239f54f552569
Baptiste Gelez <baptiste@gelez.xyz>
2019-05-01 11:54:37 +0100

    Remove some other i18n code that will be useless soon

--- config.rb
+++ config.rb
@@ -1,20 +1,5 @@
-require "i18n"
-
-I18n::Backend::Simple.include(I18n::Backend::Gettext)
-I18n.load_path << Dir["po/*.po"]  # Load all PO file in current directory
-
-activate :i18n,
-	 :langs => [:en, :es, :fr],
-	 :path => "/:locale/",
-	 :no_fallbacks => true,
-	 :data => "po"
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
-end
-
-helpers do
-  include I18n::Gettext::Helpers
 end
 
 # configure :build do
